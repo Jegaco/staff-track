16a239d922ca8c62596b4762905e0740581e71f4
Jegaco <jeffgcorpuz@gmail.com>
2023-05-31 21:37:08 -0700

    Changed data in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,21 +2,21 @@ INSERT INTO department (department_name, id)
 VALUES ("Human Resources", 1),
 ("Automotive", 2),
 ("Media", 3),
-("Deli", 4),
+("Bakery", 4),
 ("Produce", 5);
 
-INSERT INTO role (title, salary, department_id)
-VALUES ('HR Coordinator', 59,098, 1),
-('Tire Installer', 45,200, 2),
-('Social Media Manager', 70,720, 3),
-('Food Prepper', 37,025, 4),
-('Produce Stocker', 39,520, 5);
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, 'HR Coordinator', 59,098.00, 1),
+(2, 'Tire Installer', 45,200.00, 2),
+(3, 'Social Media Manager', 70,720.00, 3),
+(4, 'Cake Decorator', 37,025.00, 4),
+(5, 'Produce Stocker', 39,520.00, 5);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Mybo", "Toichi", 1),
-("Mamie", "Wilks", 2),
-("Jefferson", "Roberts", 3),
-("Arnold", "Rushing", 4),
-("Michelle", "Poli", 5),
-("Harold", "Sitz", 3),
-("Roger", "Baker", 4);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Mybo", "Toichi", 1, NULL),
+(2, "Mamie", "Wilks", 2, NULL),
+(3, "Jefferson", "Roberts", 3, NULL),
+(4, "Arnold", "Rushing", 4, 7),
+(5,"Michelle", "Poli", 5, NULL),
+(6, "Harold", "Sitz", 3, 3),
+(7, "Roger", "Baker", 4, NULL);
